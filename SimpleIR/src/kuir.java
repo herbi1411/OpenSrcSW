15d91271750a5929824d786abef50aff2e04182f
YoonMinSoo <starex2052@daum.net>
2021-04-01 10:24:11 +0900

    week5 1차완료-2

--- SimpleIR/src/kuir.java
+++ SimpleIR/src/kuir.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class kuir {
 
 	public static void main(String[] args) throws Exception {
@@ -5,34 +7,8 @@ public class kuir {
 		String dir = "src\\data";
 		String collection = "bin\\collection.xml";
 		String indexxml = "bin\\index.xml";
-		String query = "라면에는 면, 분말스프가 있다. 내일은 수요일이다. 오늘 저녁은 김치찜이다. 김치찜에는 떡먹고싶다.";
-		/*String query = "내일부터 못 보면 너는 아무렇지 않을 거니\r\n" + 
-				"안녕이란 말로만 참 쉬운 것\r\n" + 
-				"매일 너의 목소린 내게 습관보다 무서운데\r\n" + 
-				"너 혼자서 준비하고 그런 얘기 하지마\r\n" + 
-				"화를 내면 정말로 끝일 것 같아서\r\n" + 
-				"애써 나는 웃지만 눈물이 나\r\n" + 
-				"어제처럼 굿나잇 아무 일도 없던 것처럼\r\n" + 
-				"이별대신 굿나잇 내일 니 맘 바뀔지 몰라\r\n" + 
-				"하룻밤만 안녕 내일은 다 괜찮을 거야\r\n" + 
-				"다신 안 볼 사람들 하는 그 안녕이 아닌 걸지도 몰라\r\n" + 
-				"지금 내가 울면 다 끝날 것 같아서 다른 생각 해봐도 눈물이 흘러\r\n" + 
-				"어제처럼 굿나잇 아무 일도 없던 것처럼\r\n" + 
-				"이별대신 굿나잇 내일 니 맘 바뀔지 몰라\r\n" + 
-				"하룻밤만 안녕 내일은 다 괜찮을 거야\r\n" + 
-				"다신 안 볼 사람들 하는 그 안녕이 아닌 걸지도 몰라\r\n" + 
-				"헤어지잔 말은 함부로 자꾸 꺼내면 안 되는 거야\r\n" + 
-				"전부 니가 했던 말\r\n" + 
-				"이제서야 겨우 난 너를 알 것 같은데\r\n" + 
-				"사랑인 것 같은데\r\n" + 
-				"미안하단 그 말 듣지 않은 얘기로 할래\r\n" + 
-				"하룻밤만 안녕 내일은 다 괜찮을 거야\r\n" + 
-				"다신 안 볼 사람들 하는 그 안녕이 아닌 걸지도 몰라\r\n" + 
-				"하지 말아 굿바이 그런 슬픈 낯선 얼굴로\r\n" + 
-				"나를 안아 봐봐 그럼 니 맘 바뀔지 몰라\r\n" + 
-				"니가 좋아했던 표정을 나 지어볼래도\r\n" + 
-				"자꾸 못난 얼굴로 눈물이 흘러서\r\n" + 
-				"너를 볼 수 없잖아";*/
+		String query = "라면에는 면, 분말스프가 있다.";
+		String indexPost = "bin\\index.post";
 		if(args.length >0)
 		{
 			//week2////////////////////////////////////
@@ -46,14 +22,17 @@ public class kuir {
 			else if(args[0].contentEquals("-i"))
 				week4(args[1]);
 			//week5///////////////////////////////////
-			else if(args[0].contentEquals("-??"))
-				week5(args[1]);
+			else if(args[0].contentEquals("-s") && args[2].contentEquals("-q"))
+				week5(args[1],args[3]);
+			/*
+			 * Collection.xml은 class파일과 같은 경로에 있다고 가정했습니다!
+			 * */
 		}
 		else {
 		//	week2(dir);
 		//	week3(collection);
 		//	week4(indexxml);
-			week5(query);
+			week5(indexPost,query);
 		}
 	}
 	public static void week2(String dir) throws Exception{
@@ -75,11 +54,11 @@ public class kuir {
 		mp.parseDoc();
 		mp.fileOutPut();
 	}
-	public static void week5(String query) throws Exception {
-		feature ft = new feature();
-		ft.setQuery(query);
-		ft.CalcSim();
-		ft.printTop3Title();
+	public static void week5(String indexPost,String query) throws Exception{
+		searcher ft = new searcher();
+		ArrayList<Double> score;
+		score = ft.CalcSim(query,indexPost);
+		ft.printTop3Title(score);
 	}
 
 }
--- SimpleIR/src/searcher.java
+++ SimpleIR/src/searcher.java
@@ -20,35 +20,17 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-public class feature {
-	private String query;
+public class searcher {
 	private ArrayList<String> title;
 	private ArrayList<String> id;
-	private ArrayList<Double> score;
 	private String collectionFileName;
-	private String postFileName;
-	private HashMap<String,ArrayList> indexPost;
-	private KeywordList kl;
 	
-	public feature() throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException {
-		query = "";
+	public searcher() throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException {
 		title = new ArrayList<String>();
-		score = new ArrayList<Double>();
 		id = new ArrayList<String>();
-		collectionFileName = "./bin/collection.xml";
-		postFileName = "./bin/index.post";
-		
+		collectionFileName = "./collection.xml"; //collection.xml이 실행 파일과 같은 폴더에 있다고 가정
 		getTitle();
-		getIndexPost();
 	};
-	public feature(String n) throws ClassNotFoundException, ParserConfigurationException, SAXException, IOException {
-		this();
-		setQuery(n);
-	}
-	public void setQuery(String n){
-		this.query = n;
-		morphemeAnalyze();
-	}
 	private void getTitle() throws ParserConfigurationException, SAXException, IOException {
 		File file = new File(collectionFileName);
 		org.w3c.dom.Document doc;
@@ -61,23 +43,9 @@ public class feature {
 		{
 			id.add(((Node)nlist.item(i)).getAttributes().getNamedItem("id").getTextContent()); //doc id 속성값 가져오기
 			title.add(getTagValue("title",(Element)nlist.item(i)));
-			score.add(0d);
 		}
 	}
-	private void getIndexPost() throws IOException, ClassNotFoundException {
-		// TODO Auto-generated method stub
-		FileInputStream fileStream = new FileInputStream(postFileName);
-		ObjectInputStream objectInputStream = new ObjectInputStream(fileStream);
-		Object object = objectInputStream.readObject();
-		
-		indexPost = (HashMap)object;
-		Iterator<String> it = indexPost.keySet().iterator();
-		while(it.hasNext()) {
-			String key = it.next();
-			ArrayList value = (ArrayList) indexPost.get(key);
-			System.out.println(key + " -> " + value);
-		}
-	}
+
 	private String getTagValue(String tag, Element eElement) {
 	    NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
 	    Node nValue = (Node) nlList.item(0);
@@ -85,35 +53,50 @@ public class feature {
 	        return null;
 	    return nValue.getNodeValue();
 	}
-	private void morphemeAnalyze() {
-		String output ="";
-		KeywordExtractor ke = new KeywordExtractor();
-		kl = ke.extractKeyword(query, true);
-		for (int i=0; i<kl.size(); i++) {
-			Keyword kwrd = kl.get(i);
-			output += kwrd.getString() + ":" + kwrd.getCnt() + "#";
-		}
-		System.out.println(output);
-	}
-	public void CalcSim() {
+	public ArrayList<Double> CalcSim(String query, String indexPostFileName) throws ClassNotFoundException, IOException {
+		HashMap<String,ArrayList> indexPost = getIndexPost(indexPostFileName);
+		KeywordList kl = morphemeAnalyze(query);
+		ArrayList<Double> score = new ArrayList<Double>();
+		for(int i=0; i<id.size();i++) score.add(0d);
 		for(int i=0; i<id.size(); i++) {
 			String sid = id.get(i);
 			for(int j=0; j<kl.size(); j++) {
 				Keyword kwrd = kl.get(j);
 				String str = kwrd.getString();
-				//int val = kwrd.getCnt();
 				if(indexPost.containsKey(str)) {
 					if(indexPost.get(str).contains(sid)) {
-						  // score.set(i,score.get(i) + (double) val * (double)indexPost.get(str).get(indexPost.get(str).indexOf(sid)+1));
 						   score.set(i,score.get(i) + (double)indexPost.get(str).get(indexPost.get(str).indexOf(sid)+1));
-						 //  System.out.println("kwrdString :" + str + " kwrdVal :" + val + " idNum: " + sid + "idKwrdVal: " + (double)indexPost.get(str).get(indexPost.get(str).indexOf(sid)+1));
 					}
 				}
 			}
 		}
-		System.out.println(score);
+		for(int i=0; i<score.size(); i++) score.set(i, Math.round(score.get(i)*100)/100.0);
+		return score;
 	}
-	public void printTop3Title() {
+	private HashMap<String,ArrayList> getIndexPost(String indexPostFileName) throws IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		HashMap<String,ArrayList> indexPost;
+		FileInputStream fileStream = new FileInputStream(indexPostFileName);
+		ObjectInputStream objectInputStream = new ObjectInputStream(fileStream);
+		Object object = objectInputStream.readObject();
+		
+		indexPost = (HashMap)object;
+		objectInputStream.close();
+		return indexPost;
+	}
+	private KeywordList morphemeAnalyze(String query) {
+		KeywordList kl;
+		String output ="";
+		KeywordExtractor ke = new KeywordExtractor();
+		kl = ke.extractKeyword(query, true);
+		for (int i=0; i<kl.size(); i++) {
+			Keyword kwrd = kl.get(i);
+			output += kwrd.getString() + ":" + kwrd.getCnt() + "# ";
+		}
+		//System.out.println(output);
+		return kl;
+	}
+	public void printTop3Title(ArrayList<Double> score) {
 		ArrayList<Integer> rank = new ArrayList<Integer>();
 		ArrayList<String> output = new ArrayList<String>();
 		for(int i=0; i<id.size(); i++) {rank.add(1);}
@@ -131,9 +114,10 @@ public class feature {
 				}
 			}
 		}
-		System.out.println(rank);
-		System.out.println(output);
-		
+		//System.out.println("Title: " + title);
+		//System.out.println("Score:" + score);
+		//System.out.println("Rank: " + rank);
+		//System.out.println("output: " + output);
 		System.out.println(output.get(0) + ", " + output.get(1) + ", " + output.get(2));
 	}
 }
