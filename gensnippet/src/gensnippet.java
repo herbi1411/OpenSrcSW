4839e429b2cde7045f7a5ea5dcf6396d60a2636c
YoonMinSoo <starex2052@daum.net>
2021-04-20 18:59:26 +0900

    midterm2

--- gensnippet/src/gensnippet.java
+++ gensnippet/src/gensnippet.java
@@ -12,12 +12,15 @@ public class gensnippet {
 	}
 	public void analyze(String fn, String st) throws FileNotFoundException {
 		FileInputStream fi = new FileInputStream(new File(fn));;
+		int score = 0;
 		//파일 읽어오는 법을 모르겠습니다.
 		//  for (fi.hasNextLine()) 읽어올 라인이 있다면
+		score = 0;
 		String[] sp = st.split(" ");
 		String s;
 		for(int i = 0; i<sp.length; i++) {
 			s = sp[i];
+			// txt파일 라인별로 일치하면 더한다.
 		}
 	}
 }
